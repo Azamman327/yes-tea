c6f3a40be62f3eb985f1d1d8d0467f3f7b3b57cc
Azamman327 <wjsdbdud327@naver.com>
2024-10-09 23:42:42 +0900

    Design: 초기화면 UI 배치 완료

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,20 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className='container'>
+      <div className=' flex flex-col gap-20 items-center'>
       <Image
         src="/images/logo.png"
         width={250}
         height={250}
         alt="Picture of the author"
       />
-      <p>보다 간편한 차 생활</p>
+      <p className='text-lg'>보다 간편한 차 생활</p>
       <button className="bg-pointgreen rounded-full text-slate-50 w-full py-2">
         회원가입
       </button>
+      <p className='text-slate-400'>혹시 계정이 있으신가요? <a>로그인하기</a></p>
+      </div>
     </div>
   );
 }
