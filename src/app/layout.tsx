1eb49a4f882864e5d2a949bd4df27672464950d5
Azamman327 <wjsdbdud327@naver.com>
2024-10-09 21:19:38 +0900

    chore:화면 크기 설정 및 루트폴더 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=375" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
