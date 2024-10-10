65f1bb21c2f3cc7889192231bfcd8c345e3a7fc9
Azamman327 <wjsdbdud327@naver.com>
2024-10-10 16:22:07 +0900

    Refactor: Edit Indentation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,9 +13,11 @@ export default function Home() {
         alt="Picture of the author"
       />
       <p className='text-lg'>보다 간편한 차 생활</p>
-      <button className="mt-4 w-full rounded-md bg-green-500 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-green-600 focus:shadow-none active:bg-green-600 hover:bg-green-600 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" type="button">
-        회원가입
-      </button>
+      <Link href="/signup" className='w-full'>
+        <button className="mt-4 w-full rounded-md bg-green-500 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-green-600 focus:shadow-none active:bg-green-600 hover:bg-green-600 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" type="button">
+          회원가입
+        </button>
+      </Link>
       <p className='text-slate-400'>혹시 계정이 있으신가요? <Link href="/login" className='cursor-pointer'>로그인하기</Link></p>
       </div>
     </div>
