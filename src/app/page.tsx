4d01e5f0540532d5cc8a54f006e1a07c84aee413
Azamman327 <wjsdbdud327@naver.com>
2024-10-10 16:57:56 +0900

    chore: add prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,27 @@
 import '/src/styles/globals.css';
-import Image from 'next/image'
-import Link from 'next/link'
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className='container'>
-      <div className=' flex flex-col gap-14 items-center'>
-      <Image
-        src="/images/logo.png"
-        width={250}
-        height={250}
-        alt="Picture of the author"
-      />
-      <p className='text-lg'>보다 간편한 차 생활</p>
-      <Link href="/signup" className='w-full'>
-        <button className="mt-4 w-full rounded-md bg-green-500 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-green-600 focus:shadow-none active:bg-green-600 hover:bg-green-600 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" type="button">
-          회원가입
-        </button>
-      </Link>
-      <p className='text-slate-400'>혹시 계정이 있으신가요? <Link href="/login" className='cursor-pointer'>로그인하기</Link></p>
+    <div className="container">
+      <div className=" flex flex-col gap-14 items-center">
+        <Image src="/images/logo.png" width={250} height={250} alt="Picture of the author" />
+        <p className="text-lg">보다 간편한 차 생활</p>
+        <Link href="/signup" className="w-full">
+          <button
+            className="mt-4 w-full rounded-md bg-green-500 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-green-600 focus:shadow-none active:bg-green-600 hover:bg-green-600 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+            type="button"
+          >
+            회원가입
+          </button>
+        </Link>
+        <p className="text-slate-400">
+          혹시 계정이 있으신가요?
+          <Link href="/login" className="cursor-pointer">
+            로그인하기
+          </Link>
+        </p>
       </div>
     </div>
   );
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,16 +1,21 @@
 export default function () {
   return (
-    <div className='container'>
-      <div className='w-full max-w-sm min-w-[200px] mt-16'>
-        <input className='w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-full px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow' 
-          placeholder='유저 이름'
-          type='text' 
+    <div className="container">
+      <div className="w-full max-w-sm min-w-[200px] mt-16">
+        <input
+          className="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-full px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
+          placeholder="유저 이름"
+          type="text"
         />
-        <input className='w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-full px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow mt-4' 
-          placeholder='비밀번호' 
-          type='password'
+        <input
+          className="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-full px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow mt-4"
+          placeholder="비밀번호"
+          type="password"
         />
-        <button className='mt-4 w-full rounded-md bg-green-500 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-green-600 focus:shadow-none active:bg-green-600 hover:bg-green-600 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none mt-10' type="button">
+        <button
+          className="mt-4 w-full rounded-md bg-green-500 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-green-600 focus:shadow-none active:bg-green-600 hover:bg-green-600 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none mt-10"
+          type="button"
+        >
           로그인하기
         </button>
       </div>
