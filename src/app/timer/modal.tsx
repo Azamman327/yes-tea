8e47064a7390bc04c70b8fba53fc56cd0de5c8a7
Azamman327 <wjsdbdud327@naver.com>
2024-10-11 18:53:40 +0900

    Design: add timer modal

---
+++ src/app/timer/modal.tsx
@@ -0,0 +1,47 @@
+"use client"
+
+import {
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton
+} from '@chakra-ui/react'
+
+import { useDisclosure } from '@chakra-ui/react';
+
+const EditModal: React.FC = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+    return (
+      <>
+        <Button onClick={onOpen}>Open Modal</Button>
+
+        <div className='flex item-center'>
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Modal Title</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              d
+            </ModalBody>
+  
+            <ModalFooter>
+              <Button colorScheme='blue' mr={3} onClick={onClose}>
+                Close
+              </Button>
+              <Button variant='ghost'>Secondary Action</Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+        </div>
+      </>
+    )
+}
+
+export default function Component() {
+  return <EditModal />;
+}
--- src/app/timer/page.tsx
+++ src/app/timer/page.tsx
@@ -1,12 +1,15 @@
 import '/src/styles/globals.css';
 import Timer from './timer';
+import EditModal from './modal';
 
 export default function TimerMain() {
   return (
     <div className="container">
       <div className="flex flexrow justify-center font-serif text-5xl">
         <Timer />
+        
       </div>
+      <EditModal />
     </div>
   );
 }
