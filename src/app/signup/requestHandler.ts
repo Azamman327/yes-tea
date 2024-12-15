635b0d75e9b0a6444a7cc505aefc9f71e269435d
Azamman327 <wjsdbdud327@naver.com>
2024-12-15 18:06:40 +0900

    Refactor: 테스트용 코드와 파일 정리

--- src/app/signup/requestHandler.ts
+++ src/app/signup/requestHandler.ts
@@ -4,8 +4,8 @@ import axios from 'axios';
 export async function createUser(name: String, password: String, email:String): Promise<void> {
   try {
     const response = await axios.post('/api/users/create', {
-      name: 'John Doe',
-      password: 'john',
+      name: name,
+      password: password,
     });
     console.log(response.data);
   } catch (error) {
--- src/app/test/test.tsx
+++
@@ -1,4 +0,0 @@
-fetch('/test')
-  .then((res) => res.text())
-  .then((data) => console.log('Response:', data))
-  .catch((err) => console.error('Error:', err));
