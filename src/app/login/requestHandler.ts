791d674ab17cd2df8d676e7f6c26d8845532baa1
Azamman327 <wjsdbdud327@naver.com>
2024-12-21 21:07:25 +0900

    Fix: 로그인 에러 처리 수정

--- src/app/login/requestHandler.ts
+++ src/app/login/requestHandler.ts
@@ -1,20 +1,23 @@
-export async function login(name: String, password: String): Promise<void> {
-  const response = await fetch(`/api/users/login?name=${name}&password=${password}`,
-  {
-    method: 'GET',
-  });
-  const result: String = await response.json();
-  console.log(result);
+'use client';
+import axios from 'axios';
 
-  switch(result){
-    case "SUCCESS":
-      window.location.replace('/timer');
-      break;
-    case "FAILURE":
-      alert('틀린 비밀번호입니다.');
-      break;
-    case "NO_USER":
+export async function login(name: String, password: String): Promise<void> {
+  try {
+    const response = await axios.post('/api/users/login', {
+      name: name,
+      password: password,
+    });
+
+    console.log(response.data);
+    window.location.replace('/timer');
+  } catch (error) {
+    let errorMessage = error.response.data.message;
+    
+    if (errorMessage == "INVALID USER") {
       alert('존재하지 않는 회원입니다.');
-      break;
+    }
+    else if (errorMessage == "WRONG PASSWORD") {
+      alert('잘못된 비밀번호입니다.');
+    }
   }
 }
