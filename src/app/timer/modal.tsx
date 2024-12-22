ad152359731f783c935a6db3fc3da2070d12536c
Azamman327 <wjsdbdud327@naver.com>
2024-12-22 16:26:50 +0900

    Feat: timer modal chakra에서 mui로 변경

--- src/app/timer/modal.tsx
+++ src/app/timer/modal.tsx
@@ -1,58 +1,75 @@
 'use client';
 
-import {
-  Button,
-  Input,
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalFooter,
-  ModalBody,
-  ModalCloseButton,
-} from '@chakra-ui/react';
-
-import { useDisclosure } from '@chakra-ui/react';
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
 import { useTimeStore } from './timeStore';
 
 const EditModal: React.FC = () => {
   const timeStore = useTimeStore();
-  const { isOpen, onOpen, onClose } = useDisclosure();
+  
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
     <>
-      <Button onClick={onOpen} className="w-full mt-5">
+      <Button  onClick={handleClickOpen} className="w-full mt-5">
         edit time
       </Button>
 
-      <div className="flex item-center">
-        <Modal isOpen={isOpen} onClose={onClose}>
-          <ModalOverlay />
-          <ModalContent>
-            <ModalHeader>타이머 시간 바꾸기</ModalHeader>
-
-            <ModalCloseButton />
-
-            <ModalBody className="flex flex-row">
-              <span className="flex flex-row items-center pr-8 w-1/3">
-                <Input name="minute" onChange={(e) => timeStore.updateMinutesState(Number(e.target.value))} size="sm" />
-                분
-              </span>
-              <span className="flex flex-row items-center pr-8 w-1/3">
-                <Input name="second" onChange={(e) => timeStore.updateSecondsState(Number(e.target.value))} size="sm" />
-                초
-              </span>
-            </ModalBody>
-
-            <ModalFooter>
-              <Button variant="ghost" onClick={onClose}>
-                닫기
-              </Button>
-            </ModalFooter>
-          </ModalContent>
-        </Modal>
-      </div>
+      <>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        PaperProps={{
+          component: 'form',
+          onSubmit: (event: React.FormEvent<HTMLFormElement>) => {
+            event.preventDefault();
+            const formData = new FormData(event.currentTarget);
+            const formJson = Object.fromEntries((formData as any).entries());
+            const email = formJson.email;
+            console.log(email);
+            handleClose();
+          },
+        }}
+      >
+        <DialogTitle>Subscribe</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            To subscribe to this website, please enter your email address here. We
+            will send updates occasionally.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            required
+            margin="dense"
+            id="name"
+            name="email"
+            label="Email Address"
+            type="email"
+            fullWidth
+            variant="standard"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button type="submit">Subscribe</Button>
+        </DialogActions>
+      </Dialog>
+      </>
     </>
   );
 };
@@ -60,3 +77,10 @@ const EditModal: React.FC = () => {
 export default function Component() {
   return <EditModal />;
 }
+
+
+{/* <Input name="minute" onChange={(e) => timeStore.updateMinutesState(Number(e.target.value))} size="sm" />
+분
+</span>
+<span className="flex flex-row items-center pr-8 w-1/3">
+<Input name="second" onChange={(e) => timeStore.updateSecondsState(Number(e.target.value))} size="sm" /> */}
