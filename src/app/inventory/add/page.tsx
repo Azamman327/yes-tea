409ce0426268985487961c5c72b43bcb66aef478
Azamman327 <wjsdbdud327@naver.com>
2024-12-27 09:43:34 +0900

    Design: radio button mui 적용해서 value 정상적으로 전송되게 수정

--- src/app/inventory/add/page.tsx
+++ src/app/inventory/add/page.tsx
@@ -85,18 +85,16 @@ export default function inventoryAdd() {
               <TextField {...register('brand')} label="브랜드" defaultValue="" variant="standard" />
               <TextField {...register('name')} label="제품명" defaultValue="" variant="standard" />
               <div>
-              <label>
-                <input type="radio" {...register('packagingtype')} value="teabag" checked={selectedValue === "teabag"} onChange={handleChange}/>
-                티백
-              </label>
-              <label>
-                <input type="radio" {...register('packagingtype')} value="looseleaf" checked={selectedValue === "looseleaf"} onChange={handleChange}/>
-                잎차
-              </label>
-                {/* <RadioGroup {...register('packagingtype')} row>
-                  <FormControlLabel value="teabag" control={<Radio />} label="티백" defaultValue=""/>
-                  <FormControlLabel value="looseleaf" control={<Radio />} label="잎차" defaultValue="" />
-                </RadioGroup> */}
+                {/* <label>
+                  <input type="radio" {...register('packagingtype')} value="teabag" checked={selectedValue === "teabag"} onChange={handleChange}/>
+                  티백
+                </label>
+                <label>
+                  <input type="radio" {...register('packagingtype')} value="looseleaf" checked={selectedValue === "looseleaf"} onChange={handleChange}/>
+                  잎차
+                </label> */}
+                <Radio {...register('packagingtype')} checked={selectedValue === 'teabag'} onChange={handleChange} value="teabag" />
+                <Radio {...register('packagingtype')} checked={selectedValue === 'looseleaf'} onChange={handleChange} value="looseleaf" />
               </div>
               <div>
                 <span>개수 : </span>
