0fe6642e3a89c991104ec6a3b8596ece78cab4b9
Azamman327 <wjsdbdud327@naver.com>
2024-12-30 15:31:19 +0900

    Design: inventory list 예시 코드로 import만 적용

--- src/app/inventory/page.tsx
+++ src/app/inventory/page.tsx
@@ -1,11 +1,14 @@
 import '/src/styles/globals.css';
 import Sidebar from './sidebar';
+import TeaList from './table';
 
 export default function inventory() {
   return (
     <>
       <Sidebar />
-      <div className="main-container"></div>
+      <div className="main-container">
+        <TeaList />
+      </div>
     </>
   );
 }
