a57b2dfc46e629239611346a6af63cb78f082dd4
Azamman327 <wjsdbdud327@naver.com>
2025-01-03 11:21:50 +0900

    Design: inventory 삭제 checkbox 생성

--- src/app/inventory/page.tsx
+++ src/app/inventory/page.tsx
@@ -8,44 +8,13 @@ import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 
 export default function inventory(): JSX.Element {
-  const router = useRouter();
-  const [deleteState, setDeleteState] = useState(false);
+
 
   return (
     <>
       <Sidebar />
       <div className="main-container">
         <TeaList />
-        <div className="mt-5 flex justify-evenly">
-          {deleteState === true ? (
-            <>
-              <button className="mt-4 w-1/3 phone:w-1/4 rounded-md bg-red-500 py-1 px-2 sm:py-2 sm:px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg active:bg-red-700 hover:bg-red-600 active:shadow-none mt-10">
-                삭제하기
-              </button>
-              <button
-                className="mt-4 w-1/3 phone:w-1/4 rounded-md bg-slate-500 py-1 px-2 sm:py-2 sm:px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg active:bg-slate-700 hover:bg-slate-600 active:shadow-none mt-10"
-                onClick={() => setDeleteState(false)}
-              >
-                취소
-              </button>
-            </>
-          ) : (
-            <>
-              <button
-                className="mt-4 w-1/3 phone:w-1/4 rounded-md bg-green-500 py-1 px-2 sm:py-2 sm:px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg active:bg-green-700 hover:bg-green-600 active:shadow-none mt-10"
-                onClick={() => router.push('/inventory/add')}
-              >
-                추가하기
-              </button>
-              <button
-                className="mt-4 w-1/3 phone:w-1/4 rounded-md bg-slate-300 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg active:bg-slate-500 hover:bg-slate-400 active:shadow-none mt-10"
-                onClick={() => setDeleteState(true)}
-              >
-                삭제하기
-              </button>
-            </>
-          )}
-        </div>
       </div>
     </>
   );
--- src/app/inventory/teaList.tsx
+++ src/app/inventory/teaList.tsx
@@ -4,6 +4,9 @@ import { getInventoryList } from './requestHandler';
 import { useInventoryStore } from './inventoryStore';
 
 import { Stack, Table } from '@chakra-ui/react';
+import { Checkbox } from "@/components/ui/checkbox"
+
+import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 
 export default function TeaList(): JSX.Element {
@@ -31,41 +34,87 @@ export default function TeaList(): JSX.Element {
   };
 
   const inventoryStore = useInventoryStore();
+  const router = useRouter();
+  const [deleteState, setDeleteState] = useState(false);
 
   useEffect(() => {
     async function getList() {
       const list = await getInventoryList();
-      inventoryStore.initial(list);
+      inventoryStore.initial(list);      
     }
     getList();
   }, []);
 
   return (
-    <Stack gap="10">
-      <Table.ScrollArea rounded="md" height="70vh">
-        <Table.Root size="lg" variant="outline">
-          <Table.Header>
-            <Table.Row>
-              <Table.ColumnHeader>브랜드</Table.ColumnHeader>
-              <Table.ColumnHeader>제품명</Table.ColumnHeader>
-              <Table.ColumnHeader>잎차/티백</Table.ColumnHeader>
-              <Table.ColumnHeader>재고</Table.ColumnHeader>
-              <Table.ColumnHeader>유통기한</Table.ColumnHeader>
-            </Table.Row>
-          </Table.Header>
-          <Table.Body>
-            {inventoryStore.items.map((item, index) => (
-              <Table.Row key={index}>
-                <Table.Cell>{item.tea.brand}</Table.Cell>
-                <Table.Cell>{item.tea.name}</Table.Cell>
-                <Table.Cell>{item.tea.packagingtype}</Table.Cell>
-                <Table.Cell>{item.inventory.amount}</Table.Cell>
-                <Table.Cell>{item.inventory.expired}</Table.Cell>
+    <>
+      <Stack gap="10">
+        <Table.ScrollArea rounded="md" height="70vh">
+          <Table.Root size="lg" variant="outline">
+            <Table.Header>
+              <Table.Row>
+                {
+                  deleteState === true ?
+                  <Table.ColumnHeader></Table.ColumnHeader>
+                  :
+                  <></>
+                }
+                <Table.ColumnHeader>브랜드</Table.ColumnHeader>
+                <Table.ColumnHeader>제품명</Table.ColumnHeader>
+                <Table.ColumnHeader>잎차/티백</Table.ColumnHeader>
+                <Table.ColumnHeader>재고</Table.ColumnHeader>
+                <Table.ColumnHeader>유통기한</Table.ColumnHeader>
               </Table.Row>
-            ))}
-          </Table.Body>
-        </Table.Root>
-      </Table.ScrollArea>
-    </Stack>
+            </Table.Header>
+            <Table.Body>
+              {inventoryStore.items.map((item, index) => (
+                <Table.Row key={index}>
+                  {
+                    deleteState === true ?
+                    <Table.Cell className='text-center'><Checkbox variant='subtle'></Checkbox></Table.Cell>
+                    :
+                    <></>
+                  }
+                  <Table.Cell>{item.tea.brand}</Table.Cell>
+                  <Table.Cell>{item.tea.name}</Table.Cell>
+                  <Table.Cell>{item.tea.packagingtype}</Table.Cell>
+                  <Table.Cell>{item.inventory.amount}</Table.Cell>
+                  <Table.Cell>{item.inventory.expired}</Table.Cell>
+                </Table.Row>
+              ))}
+            </Table.Body>
+          </Table.Root>
+        </Table.ScrollArea>
+      </Stack>
+      <div className="mt-5 flex justify-evenly">
+        {deleteState === true ? (
+          <>
+            <button className="mt-4 w-1/3 phone:w-1/4 rounded-md bg-red-500 py-1 px-2 sm:py-2 sm:px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg active:bg-red-700 hover:bg-red-600 active:shadow-none mt-10">
+              삭제하기
+            </button>
+            <button
+              className="mt-4 w-1/3 phone:w-1/4 rounded-md bg-slate-500 py-1 px-2 sm:py-2 sm:px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg active:bg-slate-700 hover:bg-slate-600 active:shadow-none mt-10"
+              onClick={() => setDeleteState(false)}
+            >
+              취소
+            </button>
+          </>
+        ) : (
+          <>
+            <button
+              className="mt-4 w-1/3 phone:w-1/4 rounded-md bg-green-500 py-1 px-2 sm:py-2 sm:px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg active:bg-green-700 hover:bg-green-600 active:shadow-none mt-10"
+              onClick={() => router.push('/inventory/add')}
+            >
+              추가하기
+            </button>
+            <button
+              className="mt-4 w-1/3 phone:w-1/4 rounded-md bg-slate-300 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg active:bg-slate-500 hover:bg-slate-400 active:shadow-none mt-10"
+              onClick={() => setDeleteState(true)}
+            >
+              삭제하기
+            </button>
+          </>
+        )}
+      </div>
+    </>
   );
 }
